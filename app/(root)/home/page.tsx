34f63ed13520519323caabbf7a381d356e74e6b0
Nurjon <nurjon2477@gmail.com>
2024-01-08 19:38:12 +0500

    end vyorstka

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 
+import Feature from "@/components/shared/feature";
+import Footer from "@/components/shared/footer";
 import Hero from "@/components/shared/hero";
 import Navbar from "@/components/shared/navbar";
+import Statistic from "@/components/shared/statistic";
+import Testumonial from "@/components/shared/testumonial";
 import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -22,6 +26,10 @@ const Home = () => {
             <>
                   <Navbar />
                   <Hero />
+                  <Feature/>
+                  <Statistic />
+                  <Testumonial />
+                  <Footer />
             </>
       );
 };
--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -13,7 +13,8 @@ const Navbar = () => {
           
       }
   return (
-        <header className="dark:text-gray-400 dark:bg-gray-900 text-gray-900 bg-gray-200  body-font">
+        <header className=" dark:bg-gradient-to-l from-blue-900  
+        dark:text-gray-400 dark:bg-slate-900 text-gray-900 bg-gray-200 fixed w-full body-font">
               <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <a className="flex title-font font-medium items-center dark:text-white text-black mb-4 md:mb-0">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
